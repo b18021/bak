14345f2b9a7d6a842e433ade64751e8509ebb996
b18021 <e1b18021@oit.ac.jp>
2020-10-13 15:47:53 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/group9/bak/BakApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group9.bak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
